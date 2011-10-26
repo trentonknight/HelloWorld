76fe50e37f2bfc7bb66744d1b50684c33e22a67d
Jason Mansfield <trentonknight@gmail.com>
2011-10-26 16:53:15 -0400

    updated

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,5 +1,6 @@
 
 package helloworld;
+
 import java.io.* ;
 
 /**
@@ -13,14 +14,20 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+       System.out.println("Please enter your name: ");
        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
        String text = in.readLine();
+       if(!"exit".equals(text) && !"quit".equals(text)){
        System.out.println("Hello " + text + "!");
-       
+       }
+       System.out.println("Please enter your name or type exit or quit to finish: ");
        while(!"exit".equals(text) && !"quit".equals(text)){
        in = new BufferedReader(new InputStreamReader(System.in));
        text = in.readLine();
+       if(!"exit".equals(text) && !"quit".equals(text)){
        System.out.println("Hello " + text + "!");
        }
+       }
+       
     }
 }
